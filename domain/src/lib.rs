b1e1a35227c5332768439759732ffd82e2c41262
kzmake <kzmake.i3a@gmail.com>
2021-06-20 22:48:27 +0900

    export aggregate.UserBuilder

--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -3,6 +3,7 @@ pub mod repository;
 pub mod vo;
 
 pub use crate::aggregate::User;
+pub use crate::aggregate::UserBuilder;
 pub use crate::repository::IdRepository;
 pub use crate::vo::UserId;
 pub use crate::vo::UserName;
